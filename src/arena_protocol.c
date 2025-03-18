9b50d89ee91059a02cc23e2afa34046e0b4f8ea8
Derek Fox <derekefox@gmail.com>
2025-03-18 09:43:34 -0400

    Fix a couple bugs

--- src/arena_protocol.c
+++ src/arena_protocol.c
@@ -270,6 +270,7 @@ static void cmd_broadcast(player_info* player, char* msg, char* rest) {
  * removed from the playerlist and their session ending).
  */
 static void cmd_bye(player_info* player, char* arg1, char* rest) {
+  if (arg1 == NULL) arg1 = "";
   send_ok(player, "%s", arg1);
   player->state = PLAYER_DONE;
 }
